f3791dcc89d459f5d866d00b46f825ac2029a7a3
Michael Arbogast <marbogast@codingdojo.com>
2017-08-08 13:43:23 -0700

    this is my initial commit

---
+++ tester.js
@@ -0,0 +1,22 @@
+// console.log('hi there');
+
+/*
+var num = '12';
+*/
+
+// console.log(num);
+
+// for (var i = 1; i < 10; i++) {
+//     console.log(i);
+// }
+
+// let counter = 1;
+// while (counter <= 10) {
+//     console.log(counter);
+//     if (counter > 5) {
+//         counter += 1;
+//     } else {
+//         counter += 3;
+//     }
+// }
+
