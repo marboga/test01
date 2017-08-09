05dfebfca9291c595571831f3cb809057cf0f31a
Lex Myers <lex.myers@gmail.com>
2017-08-08 20:36:54 -0700

    what the f?

--- lexm13.js
+++ lexm13.js
@@ -94,7 +94,7 @@ function PrintArrayVals(arr) {
 function PrintAverageOfArray(arr) {
   sum = 0;
   for(i = 0; i < arr.length; i++) {
-    sum += arr[i];f
+    sum += arr[i];
   }
   console.log(sum / arr.length);
 }
